573d29f639449801b19ef2337e356f1a29966f6f
Jaromír Křížek <jarek.krizek@seznam.cz>
2022-09-16 22:35:02 +0200

    Naplnění prázdné databáze testovacími daty

--- Insurance_ASP/Program.cs
+++ Insurance_ASP/Program.cs
@@ -249,6 +249,12 @@ Insurance_ASP - Závìreènı projekt
       - Je doplnìna sekce s Paginací (stanovení promìnnıch prevDisabled, nextDisabled) a tlaèítka
         Pøedchozí a Další
 
+  ------------------------------------------
+- Naplnìní prázdné databáze testovacími daty
+  ------------------------------------------
+   - Zajišuje to funkce SeedEmptyDatabase
+   - Inspirováno funkcí RegisterAdmin
+
 ---------------------------------------------------------------------------------------------------
 Dotazy:
 
@@ -275,6 +281,7 @@ using Insurance_ASP.Data;
 using Insurance_ASP.Extensions;        // RegisterAdmin
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Insurance_ASP.Models;           // SeedData.Initialize(services)
 
 namespace Insurance_ASP
 {
@@ -324,6 +331,15 @@ namespace Insurance_ASP
 
             var app = builder.Build();
 
+            // Poèáteèní inicializace databáze:
+            // https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/working-with-sql?view=aspnetcore-6.0&tabs=visual-studio
+            
+            // using (var scope = app.Services.CreateScope())
+            // {
+            //     var services = scope.ServiceProvider;
+            //     SeedData.Initialize(services);
+            // }
+
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -350,7 +366,10 @@ namespace Insurance_ASP
             app.MapRazorPages();
 
             // Pøidáno kvùli registraci úètu administrátora (E-shop v ASP.NET Lekce 9)
-            await app.RegisterAdmin("admin@email.cz", "Admin123");  
+            await app.RegisterAdmin("admin@email.cz", "Admin123");
+
+            // Pokud je databáze prázdná, naplní ji poèáteèními daty.
+            await app.SeedEmptyDatabase();
 
             app.Run();
         }
